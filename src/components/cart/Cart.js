4d538fda502bf00517d0f44844806b71ae532229
Aikenkov <Fabio.rod.silva.spqr@gmail.com>
2022-09-15 21:34:38 -0300

    fix: valor total dos produtos

--- src/components/cart/Cart.js
+++ src/components/cart/Cart.js
@@ -5,44 +5,6 @@ import { getCart } from "../../service/axiosCatLove";
 import { useContext, useEffect, useState } from "react";
 import UserContext from "../../contexts/UserContext";
 
-/* const cart = [
-    {
-        name: "Ração Seca Nutrilus Pro Carne para Gatos Adultos - 10,1Kg",
-        image: "https://www.petlove.com.br/images/products/250518/small/2661128_FRENTE.jpg?1635791683",
-        price: "13671",
-        category: "rações",
-        amount: "2",
-    },
-    {
-        name: "Ração Royal Canin Sterilised para Gatos Adultos Castrados - 10,1Kg",
-        image: "https://www.petlove.com.br/images/products/224004/small/Ra%C3%A7%C3%A3o_Royal_Canin_Sterilised_para_Gatos_Adultos_Castrados_31022932.jpg?1627717840",
-        price: "44798",
-        amount: "6",
-        category: "rações",
-    },
-    {
-        name: "Ração Seca Nutrilus Pro Salmão para Gatos Adultos Castrados - 10,1Kg",
-        image: "https://www.petlove.com.br/images/products/250521/small/2661134_FRENTE.jpg?1635792669",
-        price: "15561",
-        amount: "5",
-        category: "rações",
-    },
-    {
-        name: "Ração Whiskas Carne para Gatos Adultos Castrados - 10,1Kg",
-        image: "https://www.petlove.com.br/images/products/258569/small/RAOWHI_4.JPG?1658246240",
-        price: "18999",
-        amount: "3",
-        category: "rações",
-    },
-    {
-        name: "Ração Seca Nutrilus Pro Frango para Gatos Adultos Castrados - 10,1Kg",
-        image: "https://www.petlove.com.br/images/products/250520/small/2661132_FRENTE.jpg?1635792614",
-        price: "15021",
-        amount: "1",
-        category: "rações",
-    },
-]; */
-
 const frete = 999;
 
 export default function MyCart() {
@@ -53,12 +15,8 @@ export default function MyCart() {
     myCart.forEach((e) => {
         let price = Number(e.price);
         let amount = Number(e.amount);
-
-        if (amount == 0) {
-            return;
-        }
         subtotal += amount * price;
-        console.log(amount);
+        console.log(subtotal);
     });
 
     useEffect(() => {
@@ -79,7 +37,6 @@ export default function MyCart() {
             {myCart.length > 0 ? (
                 <>
                     {myCart.map((produto, i) => {
-                        subtotal += Number(produto.price);
                         return (
                             <CartProduct index={i} produto={produto} key={i} />
                         );
@@ -102,10 +59,14 @@ export default function MyCart() {
                     <span>Frete</span>
                     <h3>R$ {(frete / 100).toFixed(2)}</h3>
                 </div>
-                <div>
-                    <span>Total</span>
-                    <h3>R$ {((frete + subtotal) / 100).toFixed(2)}</h3>
-                </div>
+                {subtotal > 0 ? (
+                    <div>
+                        <span>Total</span>
+                        <h3>R$ {((frete + subtotal) / 100).toFixed(2)}</h3>
+                    </div>
+                ) : (
+                    <></>
+                )}
             </PurchaseInfo>
             <button> Proceder para o CheckOut</button>
         </Wrapper>
