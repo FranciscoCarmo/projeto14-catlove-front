a506f8f71ee7bcd9c30c08a729011a83dabfa60e
Aikenkov <Fabio.rod.silva.spqr@gmail.com>
2022-09-17 11:52:56 -0300

    style design

--- src/components/cart/CartProduct.js
+++ src/components/cart/CartProduct.js
@@ -7,133 +7,129 @@ import { useContext } from "react";
 import { deleteProduct, postCart } from "../../service/axiosCatLove";
 
 export default function CartProduct(props) {
-  const { name, price, image, category, amount } = props.produto;
-  const { reload, setReload } = useContext(UserContext);
-  const { myCart, setMyCart } = useContext(UserContext);
-  const cart = [...myCart];
+    const { name, price, image, category, amount } = props.produto;
+    const { reload, setReload } = useContext(UserContext);
+    const { myCart, setMyCart } = useContext(UserContext);
+    const cart = [...myCart];
 
-  async function changeAmount(type) {
-    if (amount === 0 && type === "minus") {
-      return;
-    }
-    let ind = cart.indexOf(props.produto);
-    if (type === "minus") {
-      cart[ind].amount--;
-    } else {
-      cart[ind].amount++;
-    }
-    setReload(reload + 1);
-    setMyCart(cart);
-
-    postCart({ products: [...myCart] })
-      .then((res) => {
-        console.log("sucesso ao postar carrinho");
-      })
-      .catch((err) => {
-        alert("Falha ao enviar o carrinho");
-        console.log(err);
-      });
-  }
-
-  function handleDelete() {
-    deleteProduct(props.produto)
-      .then((res) => {
-        console.log(props.produto);
-        console.log("sucesso ao enviar deletar");
+    async function changeAmount(type) {
+        if (amount === 1 && type === "minus") {
+            return;
+        }
+        let ind = cart.indexOf(props.produto);
+        if (type === "minus") {
+            cart[ind].amount--;
+        } else {
+            cart[ind].amount++;
+        }
         setReload(reload + 1);
-      })
-      .catch((err) => {
-        alert("Falha ao enviar o delete");
-        console.log(err);
-      });
-  }
+        setMyCart(cart);
 
-  return (
-    <Wrapper>
-      <img src={image} />
+        postCart({ products: [...myCart] })
+            .then((res) => {
+                console.log("sucesso ao postar carrinho");
+            })
+            .catch((err) => {
+                alert("Falha ao enviar o carrinho");
+                console.log(err);
+            });
+    }
 
-      <ProductInfo>
-        <h1>{name}</h1>
-        <p>{category}</p>
-        <h2>R$ {(price / 100).toFixed(2)}</h2>
-      </ProductInfo>
-      <ButtonsSection>
-        <MdClose
-          onClick={() => {
-            handleDelete();
-          }}
-        />
-      </ButtonsSection>
+    function handleDelete() {
+        deleteProduct(props.produto)
+            .then((res) => {
+                console.log(props.produto);
+                console.log("sucesso ao enviar deletar");
+                setReload(reload + 1);
+            })
+            .catch((err) => {
+                alert("Falha ao enviar o delete");
+                console.log(err);
+            });
+    }
 
-      <div>
-        <AiOutlineMinusCircle
-          onClick={() => {
-            if (amount > 1) changeAmount("minus");
-          }}
-        />
-        <span>{amount}</span>
-        <IoIosAddCircle onClick={() => changeAmount("plus")} />
-      </div>
-    </Wrapper>
-  );
+    return (
+        <Wrapper>
+            <img src={image} />
+
+            <ProductInfo>
+                <h1>{name}</h1>
+                <p>{category}</p>
+                <h2>R$ {(price / 100).toFixed(2)}</h2>
+            </ProductInfo>
+            <ButtonsSection>
+                <MdClose
+                    onClick={() => {
+                        handleDelete();
+                    }}
+                />
+            </ButtonsSection>
+
+            <div>
+                <AiOutlineMinusCircle onClick={() => changeAmount("minus")} />
+                <span>{amount}</span>
+                <IoIosAddCircle onClick={() => changeAmount("plus")} />
+            </div>
+        </Wrapper>
+    );
 }
 
 const ProductInfo = styled.div`
-  display: flex;
-  max-width: 60%;
-  height: 80%;
-  flex-direction: column;
-  justify-content: space-around;
-  color: #1c1c1c;
+    display: flex;
+    max-width: 60%;
+    height: 80%;
+    flex-direction: column;
+    justify-content: space-around;
+    color: #1c1c1c;
 
-  h1 {
-    font-size: 12px;
-  }
-  p {
-    font-size: 12px;
-    color: #daddd8;
-  }
-  h2 {
-    font-size: 15px;
-  }
+    h1 {
+        font-size: 12px;
+    }
+    p {
+        font-size: 12px;
+        color: #daddd8;
+    }
+    h2 {
+        font-size: 15px;
+    }
 `;
 
 const Wrapper = styled.div`
-  display: flex;
-  align-items: center;
-  justify-content: space-between;
-  position: relative;
-  height: 100px;
-  width: 100%;
-  background-color: #fafaff;
-  border-radius: 8px;
-  margin-bottom: 10px;
-  padding: 0 12px;
-
-  img {
-    height: 40%;
-  }
-
-  & > :last-child {
     display: flex;
     align-items: center;
-    width: 20%;
-    justify-content: space-around;
-    font-size: 20px;
-    margin-top: 40px;
+    justify-content: space-between;
+    position: relative;
+    height: 100px;
+    width: 100%;
+    background-color: #fafaff;
+    border-radius: 8px;
+    margin-bottom: 10px;
+    padding: 0 12px;
 
-    span {
-      font-size: 8px;
-      font-weight: 700;
+    img {
+        height: 40%;
     }
-  }
+
+    & > :last-child {
+        display: flex;
+        align-items: center;
+        width: 20%;
+        justify-content: space-around;
+        font-size: 20px;
+        margin-top: 40px;
+
+        span {
+            font-size: 8px;
+            font-weight: 700;
+        }
+    }
 `;
 
 const ButtonsSection = styled.div`
-  display: flex;
-  flex-direction: column;
+    display: flex;
+    flex-direction: column;
 
-  position: absolute;
-  right: 10px;
-  top: 10px;
+    position: absolute;
+    right: 10px;
+    top: 10px;
 `;
